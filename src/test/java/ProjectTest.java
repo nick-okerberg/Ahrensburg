6aa84aee86bccf5cbe2c20a832607cf0730fe22e
Jordan Wine <jordandwine@gmail.com>
2018-04-23 09:53:51 -0700

    Update to ProjecTest to add authentication to prevent Travis from getting rate-limited

--- src/test/java/ProjectTest.java
+++ src/test/java/ProjectTest.java
@@ -2,16 +2,44 @@ package test.java;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+
 import org.json.JSONException;
 import org.junit.Test;
 import org.junit.Before;
+import org.junit.BeforeClass;
+
 import main.java.memoranda.Project;
 import main.java.memoranda.ProjectImpl;
+import main.java.memoranda.util.JsonApiClass;
+import main.java.memoranda.util.Util;
 import nu.xom.Attribute;
 import nu.xom.Element;
 
 public class ProjectTest {
   Project prj;
+  
+  /**
+   * Builds a JsonApiClass based on Jordan Wine's SER316 repo to use to test.
+   */
+  @BeforeClass
+  public static void authSetup() {
+      try {
+          new File(Util.getEnvDir()).mkdirs();
+          File file = new File(Util.getEnvDir()+"authencoded.txt");
+          FileWriter writer = new FileWriter(file);
+          String userCredentials = "api-test-user-123:test123";
+          String basicAuth = "Basic " + java.util.Base64.getEncoder().encodeToString(userCredentials.getBytes());
+          writer.write(basicAuth);
+          writer.close();
+      } catch (IOException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+      }
+  }
   
   @Before
   public void setup() {
